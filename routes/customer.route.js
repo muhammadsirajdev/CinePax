33b310d25a95b6566de6b2a489587bd7a7ecfb80
Muhammad Siraj <FA23-BCS-077@ISBSTUDENT.COMSATS.EDU.PK>
2025-05-26 23:05:30 +0500

    Update customer.route.js

--- routes/customer.route.js
+++ routes/customer.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {verifyToken,isAdmin} = require('../middleware/auth.middleware');
-// Example: import controllers to handle customer routes
+
 const { getProfile } = require('../controllers/customer.controller');
 
 router.get('/profile', verifyToken, getProfile);
